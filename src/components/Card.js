79ae6a69be8562881c925219d83bbc12443a2357
José Santillano (Harvey) <76187688+Jose-Santillano@users.noreply.github.com>
2023-04-04 02:08:34 -0600

    Bugs arreglados
    Se arreglarón los siguientes bugs:
    
    - Componente de Moves y Timer no tenían buen diseño responsivo (se sobreescribian por debajo de las cartas)
    
    - Componente GameScreen arreglado, se implementó código para asegurar los clicks y que no se pudieran elegir todas las cartas en menos de 1 segundo. (solo se pueden dos clicks cada 2 segundos para asegurar la animación y el bindeo de cartas)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 export default function Card(props) {
+
     return (
         <div
             className={`card ${props.rotate ? 'rotate' : ""}`}
--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -13,45 +13,64 @@ export default function GameScreen(props) {
     //Variables de estado
     const [cardsArr, setCardsArr] = useState([]);
     const [moves, setMoves] = useState(0);
+    //  Control de clicks.
+    const [clicksAmount, setClicksAmount] = useState(0);
 
     //Generamos una unica ves el array aleatorio
     useEffect(() => {
         setCardsArr(arrCardRand(props.numCard));
     }, [props.numCard]);
 
+    useEffect(() => {
+        console.log(clicksAmount);
+        if(clicksAmount>=2){
+            setTimeout(() => setClicksAmount(0), 2000);
+            console.log("reinicio de clicks.");
+        };
+    }, [clicksAmount])
+
     //Funcion para rotar la tarjeta
     const rotate = (id, set) => {
-        if (set === 0) {
-            setCardsArr(prevArr => {
-                prevArr[id].rotate = true;
-                prevArr[id].validating = 1; //Indica que este elemento que esta siendo evaluado.
-                return [...prevArr];
-            });
-            setTimeout(() => {
-                validate();
-            }, 1000);
+
+        if(clicksAmount<2){    
+
+            //Aumentamos limite de clicks por cada movimiento.
+            setClicksAmount(clicksAmount + 1);
+
+            //Aumentamos la cantidad de movimientos por cada rotación.
+            setMoves(moves + 1);
+
+            if (set === 0) {
+                setCardsArr(prevArr => {
+                    prevArr[id].rotate = true;
+                    prevArr[id].validating = 1; //Indica que este elemento que esta siendo evaluado.
+                    return [...prevArr];
+                });
+                setInterval(() => validate(), 2000);
+            }
         }
     }
 
     //Funcion para validar las tarjetas (iguales o no)
     const validate = () => {
 
-       
-
         //Obtenemos los dos elementos filtrandolos.
         const validatingCards = cardsArr.filter(card => card.validating === 1);
-
-        setMoves(moves + 1);
+        
         if (validatingCards.length === 2) {
             if (validatingCards[0].bind !== validatingCards[1].bind) {
                 //La validacion no coincide
-                setCardsArr(prevArr => {
-                    prevArr[validatingCards[0].id].rotate = false;
-                    prevArr[validatingCards[0].id].validating = 0;
-                    prevArr[validatingCards[1].id].rotate = false;
-                    prevArr[validatingCards[1].id].validating = 0;
-                    return [...prevArr];
-                });
+                //Le damos tiempo de 1 segundo para que valide mas lento cuando
+                //son cartas incorrectas.
+                setTimeout(() => {
+                    setCardsArr(prevArr => {
+                        prevArr[validatingCards[0].id].rotate = false;
+                        prevArr[validatingCards[0].id].validating = 0;
+                        prevArr[validatingCards[1].id].rotate = false;
+                        prevArr[validatingCards[1].id].validating = 0;
+                        return [...prevArr];
+                    });
+                }, 1000);
             } else {
                 //La validacion coincide
                 setCardsArr(prevArr => {
@@ -65,9 +84,10 @@ export default function GameScreen(props) {
         }
     }
 
+    //Mostramos la pantalla final cuando el juego se completa.
     useEffect(() => {
         const arraySet1 = cardsArr.filter(element => element.set === 1);
-        if (arraySet1.length === props.numCard) props.setFinish(2);
+        if (arraySet1.length === props.numCard) setTimeout(() => props.setFinish(2), 1000);
     }, [cardsArr]);
     
     return (
@@ -77,7 +97,7 @@ export default function GameScreen(props) {
                     <p>Moves: {moves}</p>
                 </div>
                 <div className="gamescreen--time text-right">
-                    <p>Time: {convertTimer(props.time)}</p>
+                    <p className="">Time: {convertTimer(props.time)}</p>
                 </div>
             </div>
             <div className="gamescreen--cards grid grid-4">
